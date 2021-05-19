985c1b156ac5b358884bfa332c95f91b1b9054a4
visakh427799 <visakhsanthosh69@gmail.com>
2021-05-19 09:25:46 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,58 @@
 import "./App.css";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 import { useState } from "react";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("✏️ Add item... ");
-  const [multiple,setMultiple]=useState(false);
-  const [edit,setEdit]=useState({
-    id:'',
-    status:false
-  })
+  const [multiple, setMultiple] = useState(false);
+  const [edit, setEdit] = useState({
+    id: "",
+    status: false,
+  });
   const handleChange = (e) => {
     setTodo(e.target.value);
   };
 
   const handleSubmit = () => {
-    if (todo !== "" && todo!=="✏️ Add item...") {
+    if (todo !== "" && todo !== "✏️ Add item...") {
       setTodos([...todos, { id: Date.now(), text: todo, selected: false }]);
-      setTodo('')
+      setTodo("");
     } else {
       Swal.fire({
-        position: 'top-end',
-        icon: 'error',
-        title: 'Please provide an input',
+        position: "top-end",
+        icon: "error",
+        title: "Please provide an input",
         showConfirmButton: false,
-        timer: 1500
-      })
+        timer: 1500,
+      });
     }
   };
 
-const handleEdit=()=>{
-  
-  let id = edit.id;
-  let newTodos = todos.filter((todo2) => {
-    if (todo2.id === id) {
-      todo2.text = todo;
-    }
-    return todo2;
-  });
+  const handleEdit = () => {
+    let id = edit.id;
+    let newTodos = todos.filter((todo2) => {
+      if (todo2.id === id) {
+        todo2.text = todo;
+      }
+      return todo2;
+    });
 
-  setTodos(newTodos);
-  setEdit({id:'',status:false});
-  setTodo('')
+    setTodos(newTodos);
+    setEdit({ id: "", status: false });
+    setTodo("");
+  };
 
-}
-
-const handleMultiple=()=>{
-    let newArr=todos.filter((val)=>{
-        if(!val.selected){
-          return val
-        }
-    })
+  const handleMultiple = () => {
+    let newArr = todos.filter((val) => {
+      if (!val.selected) {
+        return val;
+      }
+    });
     console.log(newArr);
-     setTodos(newArr)
-     setMultiple(false)
-}
+    setTodos(newArr);
+    setMultiple(false);
+  };
 
   return (
     <div className="app">
@@ -73,10 +71,11 @@ const handleMultiple=()=>{
           placeholder="✏️ Add item... "
         />
 
-        {
-        edit.status?<i onClick={handleEdit} class="far fa-save"></i>:  <i onClick={handleSubmit} className="fas fa-plus-circle"></i>
-        }
-      
+        {edit.status ? (
+          <i onClick={handleEdit} class="far fa-save"></i>
+        ) : (
+          <i onClick={handleSubmit} className="fas fa-plus-circle"></i>
+        )}
       </div>
       <div className="todos">
         {todos.map((todo, key) => {
@@ -96,17 +95,14 @@ const handleMultiple=()=>{
                     setTodos(newTodos);
                     console.log(newTodos);
 
-                    let multiple=todos.filter((item)=>{
-                      if(item.selected) return item
-                  })
+                    let multiple = todos.filter((item) => {
+                      if (item.selected) return item;
+                    });
 
-                    if(multiple.length>1){
-                      setMultiple(true)
-                   
-                    }
-                    else{
-                      setMultiple(false)
-
+                    if (multiple.length > 1) {
+                      setMultiple(true);
+                    } else {
+                      setMultiple(false);
                     }
                   }}
                   type="checkbox"
@@ -116,26 +112,24 @@ const handleMultiple=()=>{
                 <p>{todo.text}</p>
               </div>
               <div className="right">
-              <i onClick={()=>{
-                  setTodo(todo.text);
-                  setEdit({id:todo.id,status:true})
-
-              }
-
-              }class="far fa-edit"></i> <i
+                <i
+                  onClick={() => {
+                    setTodo(todo.text);
+                    setEdit({ id: todo.id, status: true });
+                  }}
+                  class="far fa-edit"
+                ></i>{" "}
+                <i
                   onClick={(e) => {
                     let newTodos = todos.filter((todo2) => {
-                      if (todo2!== todo) {
+                      if (todo2 !== todo) {
                         return todo2;
+                      } else {
+                        return null;
                       }
-                      else{
-                        return null
-                      }
-                     
                     });
-                    
-                    setTodos(newTodos);
 
+                    setTodos(newTodos);
                   }}
                   className="far fa-trash-alt"
                 ></i>
@@ -144,11 +138,11 @@ const handleMultiple=()=>{
           );
         })}
       </div>
-       {
-        multiple?<i onClick={handleMultiple}className="far fa-trash-alt"
-        >Delete Selected</i>:null
-      }
-     
+      {multiple ? (
+        <i onClick={handleMultiple} className="far fa-trash-alt">
+          Delete Selected
+        </i>
+      ) : null}
     </div>
   );
 }
